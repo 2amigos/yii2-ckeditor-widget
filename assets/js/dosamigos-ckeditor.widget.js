a2a00ce3709603131e3bf01e76509221988c94b8
Antonio Ramirez <amigo.cobos@gmail.com>
2014-07-18 20:25:52 +0200

    minor fix

--- assets/js/dosamigos-ckeditor.widget.js
+++ assets/js/dosamigos-ckeditor.widget.js
@@ -24,7 +24,7 @@ dosamigos.ckEditorWidget = (function ($) {
                     var csrfTokenInput = $('<input/>').attr({'type': 'hidden', 'name': yii.getCsrfParam()}).val(yii.getCsrfToken());
                     $form.append(csrfTokenInput);
                 }
-            })
+            });
         }
     };
     return pub;
