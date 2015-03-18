84ed6fc3d55a8beb2ba937a6b1f2a27f3ab5f4ca
Antonio Ramirez <antonio@ramirezcobos.com>
2015-03-18 22:04:22 +0100

    Update TestCKEditorWidgetAsset.php

--- tests/functional/data/overrides/TestCKEditorWidgetAsset.php
+++ tests/functional/data/overrides/TestCKEditorWidgetAsset.php
@@ -1,14 +1,4 @@
 <?php
-/**
- *
- * TestCKEditorWidgetAsset.php
- *
- * Date: 02/03/15
- * Time: 12:24
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
- */
 
 namespace tests\data\overrides;
 
