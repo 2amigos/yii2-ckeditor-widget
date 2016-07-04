6d5ba4db94939c5f29451df363c9f6afd9740779
mbv <terehovk@ya.ru>
2016-07-04 16:54:30 +0300

    Fix bug with csrf token

--- src/assets/dosamigos-ckeditor.widget.js
+++ src/assets/dosamigos-ckeditor.widget.js
@@ -18,7 +18,7 @@ dosamigos.ckEditorWidget = (function ($) {
             });
         },
         registerCsrfImageUploadHandler: function () {
-            yii & $(document).off('click', '.cke_dialog_tabs a:eq(2)').on('click', '.cke_dialog_tabs a:eq(2)', function () {
+            yii & $(document).off('click', '.cke_dialog_tabs a:eq(1), .cke_dialog_tabs a:eq(2)').on('click', '.cke_dialog_tabs a:eq(1), .cke_dialog_tabs a:eq(2)', function () {
                 var $form = $('.cke_dialog_ui_input_file iframe').contents().find('form');
                 var csrfName = yii.getCsrfParam();
                 if (!$form.find('input[name=' + csrfName + ']').length) {
